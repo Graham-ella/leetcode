ab0b6c32c027eddd41fed3d16462f103e5a60665
Graham <3235962608@qq.com>
2021-09-06 20:39:32 +0800

    complete

---
+++ 每日一题/lc21合并两个有序链表.cpp
@@ -0,0 +1,35 @@
+/*
+合并两个有序链表
+先新建一个头结点会比较好操作
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(-1);
+        ListNode* temp = dummy;
+        while(l1 && l2){
+            if(l1->val < l2->val){
+                temp->next = l1;
+                l1 = l1->next;
+            }else{
+                temp->next = l2;
+                l2 = l2->next;
+            }
+            temp = temp->next;
+        }
+
+        temp->next = (l1)l1:l2;
+        return dummy->next;
+    }
+};
